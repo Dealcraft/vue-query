971e748be582c06e33fea14335977f17c3fa51c9
David <david.loh18.dl@gmail.com>
2023-12-18 21:32:31 +0100

    removed unused authentication settings

--- src/lib/types/PluginOptions.ts
+++ src/lib/types/PluginOptions.ts
@@ -1,10 +1,8 @@
-import { AuthenticationStrategy } from "../strategies/AuthenticationStrategy.ts";
 import { Logger } from "./Logger.ts";
 import { LogLevel } from "./../constants/LogLevel.ts";
 
 export type PluginOptions = {
 	baseUrl?: string;
-	authentication?: AuthenticationStrategy;
 	logger?: Logger;
 	logLevel?: LogLevel;
 };
